ce542f354048f062edbb11646af71103e0a6aa3e
alterkait <alterkait2001@icloud.com>
2017-09-13 22:51:47 +0300

    Create precompiled.h

---
+++ PhysicsEngine/precompiled.h
@@ -0,0 +1,17 @@
+#ifndef PRECOMPILED_H
+#define PRECOMPILED_H
+
+#if defined 
+  #ifndef WIN32
+  #define WIN32
+  #endif
+#endif
+
+#include <cstring>
+#include <cstdlib>
+#include <cfloat>
+#include <vector>
+
+
+#include "glut.h"
+#include "IEmath.h"

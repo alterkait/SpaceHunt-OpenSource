cfe7ad732a8d8df0831bcbf00cd21c0e096c9969
alterkait <alterkait2001@icloud.com>
2017-09-13 21:31:37 -0800

    Update Collision.cpp

--- PhysicsEngine/Collision.cpp
+++ PhysicsEngine/Collision.cpp
@@ -2,5 +2,37 @@
 
 CollisionCallBack Dispatch[Shape::eCount][Shape::eCount] =
 {
+  {
+      CircleToCircle, CircleToPolygon
+  },
+  {
+    PolygonToCircle, PolygonToPolygon
+  },
+};
+
+void CircleToCircle (Manifold *m, Body *a, Body *b)
+{
+  Circle *A = reinterpret_cast<Circle *>;
+  Circle *B = reinterpret_cast<Circle *>;
   
+  Vec2 normal = b->position - a->position;
+  
+    if(dist_sqr >= radius * radius)
+      {
+        return 0;
+      }
+  real distance;
+  distance = std::sqrt (dist_sqrt);
+    
+    if(distance == 0.f)
+      {
+        m->normal = Vec2 (1, 0);
+      } 
+        else
+        {
+          m -> normal;
+          m -> normal = normal / distance; 
+        }
 }
+
+

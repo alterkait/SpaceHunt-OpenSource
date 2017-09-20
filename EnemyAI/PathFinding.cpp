bd77b6ba95fa413ea353eaeecaff5202baf83168
alterkait <alterkait2001@icloud.com>
2017-09-20 14:24:02 +0300

    Update PathFinding.cpp

--- EnemyAI/PathFinding.cpp
+++ EnemyAI/PathFinding.cpp
@@ -2,6 +2,39 @@
 
 struct PathNode
 {
-PathNode ()
+  PathNode ()
+    {
+      G = 0;
+      H = 0;
+      open = false;
+      closed = false;
+    }
+  float G;
+  float H;
+  bool open;
+  bool closed;
   
-}
+};
+
+const int nodeGridSize = 100;
+PathNode nodeGrid;
+
+PathNode* GetGrid(const sf::Vector _pos, int width)
+  {
+    return 1;
+  }
+
+  PathNode* GetGrid(int x, int y, _width)
+  {
+    return 1;
+  }
+
+class PriorityQueue
+{
+  public:
+    struct PriorityQueueItem
+    {
+      sf::vector pos;
+      float priority;
+      
+    };

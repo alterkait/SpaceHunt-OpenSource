d34b8377f0bce1a503f735c89e52c01222fb881c
alterkait <alterkait2001@icloud.com>
2017-09-20 14:13:24 +0300

    Update projectile.cpp

--- EnemyAI/projectile.cpp
+++ EnemyAI/projectile.cpp
@@ -1,1 +1,25 @@
 #include "projectile.h" 
+
+Projectile::Projectile()
+{
+  PrimaryActorTick.EveryTick = true;
+  
+  ProjectileMovement = SubObject <ProjectileComponent> (Name("Projectile Movement"));
+  ProjectileMovement = false;
+}
+
+void Projectile::BeginPlay()
+{
+  BeginPlay();
+}
+
+void Projectile:Tick (float DeltaTime)
+{
+  (DeltaTime);
+}
+
+Projectile::LaunchProjectile (float speed)
+{
+  ProjectileMovement -> SetV(Vector::ForwardVector * speed);
+  ProjectileMovement -> Activate();
+}
